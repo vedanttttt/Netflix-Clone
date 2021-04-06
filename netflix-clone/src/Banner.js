1cb48ba280494d65ec4e016e259cabe3dd1c60f0
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2021-04-06 16:01:58 +0530

    Nav.js

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -21,6 +21,10 @@ function Banner() {
 
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -32,15 +36,21 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
 
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
 
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
--- netflix-clone/src/requests.js
+++ netflix-clone/src/requests.js
@@ -1,10 +1,10 @@
-const API_KEY = 'f6dd029776064a394e4a9101a6697315';
+const API_KEY = "f6dd029776064a394e4a9101a6697315";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with-genres=10749`,
